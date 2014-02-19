730e7d7d3c01ec61082c64ebde61b4c6007d94ce
Winston Weinert <winston@ml1.net>
2014-02-19 13:40:10 -0600

    added redis cache & add verbose logging

--- app.py
+++ app.py
@@ -1,18 +1,38 @@
 from flask import Flask, render_template, request
 import imgur_game
+from store import redis, redis_url
+import sys
 
-
+TTL = 60 * 60 * 24
 app = Flask(__name__)
 
 @app.route('/')
 def game():
     s = request.args.get('s', '')
+    canonical_s = s.lower()
+    log('Redis url {}'.format(redis_url))
+    log('Processing {} canonical {}'.format(s, canonical_s))    
     if len(s) == 5:
-        urls = imgur_game.imgur_game2(s)
+        url_s = redis.get(canonical_s)
+        log('Passed length check. Redis reports {}.'.format(url_s))        
+        if not url_s:
+            log('Fetching images, not found in cache.')            
+            urls = list(imgur_game.imgur_game2(s))
+            log('Fetched urls {} in list'.format(len(urls)))
+            redis.setex(canonical_s, urls, TTL)
+            log('Saved to Redis')
+        else:
+            log('Found in cache.')            
+            urls = eval(url_s)
     else:
         urls = []
+    log('Rendering template imgur.html')
     return render_template('imgur.html', urls=urls, s=s)
 
+
+def log(*args):
+  print args[0] % (len(args) > 1 and args[1:] or [])
+  sys.stdout.flush()
 
 if __name__ == '__main__':
     app.debug = True
